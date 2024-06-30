ba332d5220991b15fd66db3d98e12df824582139
Frounzy <erolgblk2727@gmail.com>
2024-06-30 14:17:23 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const buton = document.querySelector("#btnBot");
+buton.addEventListener("click", function () {
+  alert(
+    "Discord Bot Sistemim Bakım Aşamasında   |  https://discord.gg/C6cwnWXprb."
+  );
+});
+
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", function () {
+  alert(
+    "Projelerimi Görmek İçin Discord Gel.  |  https://discord.gg/C6cwnWXprb"
+  );
+});
